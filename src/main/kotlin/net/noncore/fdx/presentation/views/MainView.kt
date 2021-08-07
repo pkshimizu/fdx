ef195b6c18e5596e1ab523659e360f7000bf2dd9
shimizu.kenji <shimizu.kenji@noncore.net>
2021-08-07 19:57:14 +0900

    fxmlを追加

--- src/main/kotlin/net/noncore/fdx/presentation/views/MainView.kt
+++ src/main/kotlin/net/noncore/fdx/presentation/views/MainView.kt
@@ -1,16 +1,8 @@
 package net.noncore.fdx.presentation.views
 
+import javafx.scene.layout.AnchorPane
 import tornadofx.*
 
 class MainView : View("Main View") {
-
-    override val root = borderpane {
-        center = label {
-            text = "fdx ver 1.0.0"
-            maxHeight = 16.0
-        }
-        right = label { text = "2021-07-23 17:47"}
-        prefHeight = 16.0
-        maxHeight = 16.0
-    }
+    override val root: AnchorPane by fxml("/MainView.fxml")
 }
--- src/main/kotlin/net/noncore/fdx/presentation/views/SampleView.kt
+++
@@ -1,8 +0,0 @@
-package net.noncore.fdx.presentation.views
-
-import javafx.scene.layout.BorderPane
-import tornadofx.*
-
-class SampleView : View("My View") {
-    override val root: BorderPane by fxml()
-}
