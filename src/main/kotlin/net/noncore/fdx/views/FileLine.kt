ab55d2c045a9956df40b04cc9a91ab6f2331453e
shimizu.kenji <shimizu.kenji@noncore.net>
2021-08-14 13:08:04 +0900

    FileLineを追加

---
+++ src/main/kotlin/net/noncore/fdx/views/FileLine.kt
@@ -0,0 +1,8 @@
+package net.noncore.fdx.views
+
+import javafx.scene.layout.AnchorPane
+import tornadofx.View
+
+class FileLine : View("File Line") {
+    override val root: AnchorPane by fxml("/FileLine.fxml")
+}
