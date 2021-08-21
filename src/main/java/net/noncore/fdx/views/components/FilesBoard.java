250087598303e588d0f82e01c6b5b36198e5489b
shimizu.kenji <shimizu.kenji@noncore.net>
2021-08-21 15:51:01 +0900

    高さに合わせて、FileLineをレイアウトするように対応してみた 独自のレイアウトを使うよりもTableを使ったほうが良いかも

--- src/main/java/net/noncore/fdx/views/components/FilesBoard.java
+++ src/main/java/net/noncore/fdx/views/components/FilesBoard.java
@@ -5,15 +5,44 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import net.noncore.fdx.views.View;
 
+import java.util.List;
+
 public class FilesBoard extends GridPane implements View {
     @FXML
     private VBox left;
     @FXML
     private VBox right;
+    private int lineCount = 0;
+    private List<FileLine> lines = List.of(
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine(),
+            new FileLine());
 
    public FilesBoard() {
        load("FilesBoard.fxml");
-       left.getChildren().add(new FileLine());
-       right.getChildren().add(new FileLine());
+       heightProperty().addListener(((observable, oldValue, newValue) -> {
+           int count = newValue.intValue() / 32;
+           if (lineCount < count) {
+               if (lines.size() > lineCount) {
+                   for(int index = lineCount; index < count; index++) {
+                       left.getChildren().add(lines.get(index));
+                       right.getChildren().remove(lines.get(index));
+                   }
+                   for (int index = count; index < lines.size(); index++) {
+                       if (right.getChildren().indexOf(lines.get(index)) < 0) {
+                           right.getChildren().add(lines.get(index));
+                       }
+                   }
+               }
+           }
+           lineCount = count;
+       }));
    }
 }
