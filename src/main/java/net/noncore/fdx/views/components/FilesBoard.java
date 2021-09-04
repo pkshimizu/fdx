0793f0613631fb9db1d441b6346a5ea9dea81f02
shimizu.kenji <shimizu.kenji@noncore.net>
2021-09-04 20:35:22 +0900

    ユーザーホームディレクトリのファイルを表示してみた クラス構成の整理が必要

--- src/main/java/net/noncore/fdx/views/components/FilesBoard.java
+++ src/main/java/net/noncore/fdx/views/components/FilesBoard.java
@@ -1,21 +1,47 @@
 package net.noncore.fdx.views.components;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import net.noncore.fdx.views.View;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
 public class FilesBoard extends AnchorPane implements View {
     @FXML
     private TableView<FileItem> table;
 
+    private static final List<String> FILE_SIZE_UNITS = Arrays.asList("Bite", "KB", "MB", "GB", "TB");
+
    public FilesBoard() {
        load("FilesBoard.fxml");
-       table.setItems(FXCollections.observableArrayList(
-               new FileItem("build.gradle", "1KB", "2021-08-17 08:50"),
-               new FileItem("gradlew", "6KB", "2021-08-16 22:11"),
-               new FileItem("gradlew.bat", "3KB", "2021-08-16 22:11"),
-               new FileItem("settings.gradle", "24B", "2021-08-16 22:11")));
+       try {
+           Files.list(Path.of(System.getProperty("user.home"))).forEach(path -> {
+               try {
+                   Path name = path.getFileName();
+                   long size = Files.size(path);
+                   LocalDateTime date = Files.getLastModifiedTime(path).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+                   table.getItems().add(new FileItem(name.toString(), toSizeText(size, FILE_SIZE_UNITS), date.format(DateTimeFormatter.ISO_DATE)));
+               } catch (IOException e) {
+                   throw new RuntimeException(e);
+               }
+           });
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+   }
+
+   private String toSizeText(long size, List<String> units) {
+        if (size < 1024) {
+            return String.format("%d %s", size, units.get(0));
+        }
+        return toSizeText(size / 1024, units.subList(1, units.size()));
    }
 }
